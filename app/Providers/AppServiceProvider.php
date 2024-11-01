de0f09e7f959150eecf4994892129e8c7ead1953
michailtjhang <michailaja@gmail.com>
2024-11-01 17:01:25 +0700

    refactor: fix error clockwork

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\File;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
     {
         if (env('APP_ENV') != 'local') {
             URL::forceScheme('https');
+        }
+        $clockworkPath = '/tmp/clockwork';
+        if (!File::exists($clockworkPath)) {
+            File::makeDirectory($clockworkPath, 0777, true);
         }
     }
 }
